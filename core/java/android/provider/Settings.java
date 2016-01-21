9ed2bb5c07abf65e83e90b672de97071d09a8208
Alex Cruz <mazdarider23@gmail.com>
2016-01-21 11:14:04 -0500

    Configurable settings dashboard modifications [2/2]
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3392,6 +3392,22 @@ public final class Settings {
          */
 
         /**
+         * Change the number of columns displayed in the Settings dashboard
+         * @hide
+         */
+        public static final String DASHBOARD_COLUMNS = "dashboard_columns";
+
+        /**
+         * Whether to display dashboard tiles in a double line layout
+         * @hide
+         */
+        public static final String DASHBOARD_TILEVIEW_DOUBLE_LINES = "dashboard_tileview_double_lines";
+
+        /**
+         * Whether to display dashboard dividers
+         * @hide
+         */
+        public static final String DASHBOARD_TILEVIEW_DIVIDERS = "dashboard_tileview_dividers";
 
        /**
         * MediaScanner behavior on boot.
